745b46a6effe5a887ae4a8e6d6aabc914d3dfb9e
Vivek Jadeja <Vivek.Jadeja98@gmail.com>
2020-09-14 12:44:54 -0500

    Updates

---
+++ precache-manifest.8a36be26916ce1dc7abf1e90c54e9d8b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e86b1a53a80fa57e6abcc2ab3b546056",
+    "url": "/VivekJadeja.github.io/index.html"
+  },
+  {
+    "revision": "d88d13444a5527b7f6a2",
+    "url": "/VivekJadeja.github.io/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "0385abb4797200db2b4d",
+    "url": "/VivekJadeja.github.io/static/css/main.748b22ac.chunk.css"
+  },
+  {
+    "revision": "d88d13444a5527b7f6a2",
+    "url": "/VivekJadeja.github.io/static/js/2.5de86346.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/VivekJadeja.github.io/static/js/2.5de86346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0385abb4797200db2b4d",
+    "url": "/VivekJadeja.github.io/static/js/main.0f000217.chunk.js"
+  },
+  {
+    "revision": "7b897e4241aa10e8358f",
+    "url": "/VivekJadeja.github.io/static/js/runtime-main.cf08b16c.js"
+  },
+  {
+    "revision": "ba0420e03902cb24b80753d2f2e259aa",
+    "url": "/VivekJadeja.github.io/static/media/FoodPic.ba0420e0.JPEG"
+  },
+  {
+    "revision": "f20b6669437402cd32a1045f56341f8e",
+    "url": "/VivekJadeja.github.io/static/media/ResumeSS.f20b6669.png"
+  },
+  {
+    "revision": "59a6979d4be1bebb30841252c38613b9",
+    "url": "/VivekJadeja.github.io/static/media/ghubPic.59a6979d.png"
+  }
+]);
