38bc25b65bddc2d750d54709dd1579b452d1c4eb
Vivek Jadeja <Vivek.Jadeja98@gmail.com>
2020-09-23 23:27:02 -0500

    change

--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { useSpring, animated } from 'react-spring';
 import { LinkContainer } from 'react-router-bootstrap';
-import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
-import {NavLink} from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
 
 
 function CardInfo(props) {
@@ -16,9 +15,11 @@ function CardInfo(props) {
         <animated.div className="v-card-info" style={style}>
             <p className="v-card-title">{props.title}</p>
             {props.isPage ?
-                <NavLink className="v-card-subTitle" to={props.link} target="_blank" rel="noopnener noreferrer">
-                    {props.subTitle}
-                   </NavLink> :
+                <LinkContainer className="v-card-subTitle" to={props.link} target="_blank" rel="noopnener noreferrer">
+                    <Nav.Link>
+                        {props.subTitle}
+                    </Nav.Link>
+                </LinkContainer> :
                 <a className="v-card-subTitle" href={props.link} target="_blank" rel="noopnener noreferrer">{props.subTitle}</a>
             }
             {/*this <a></a> above is an anchor tag for a link*/}
